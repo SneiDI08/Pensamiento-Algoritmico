3ebdc5e52d97d15e07c0f8e44ec418f5b2692feb
SneiDI08 <sneidi200810@gmail.com>
2025-02-17 07:22:28 -0500

    Add files via upload

---
+++ ProgramaUni3.py
@@ -0,0 +1,13 @@
+a:int = 10
+
+print (f"Mira el numero {a}")
+a+=int(5)
+print (f"Aumentamos con 5: {a}")
+a-=int(3)
+print (f"Reducimos en 3: {a}")
+a*=int(2)
+print (f"Multiplicamos por 2: {a}")
+a/=int(4)
+print (f"Y lo dividimos entre 4: {a}")
+
+print (f"El resultado final es: {a}")
