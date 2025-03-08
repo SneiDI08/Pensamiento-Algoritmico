1a97db2ed1e4625d090d095e23cb11406b16b6fd
SneiDI08 <sneidi200810@gmail.com>
2025-03-07 20:48:50 -0500

    Update proyectiles.cpp

--- Primer Semestre/Parciales/Primer Corte/proyectiles.cpp
+++ Primer Semestre/Parciales/Primer Corte/proyectiles.cpp
@@ -1,56 +1,48 @@
 #include <iostream>
-using namespace std; //El using namespace std se usa para no tener que escribir std:: antes de elementos que pertenecen al espacio de nombres
+using namespace std;//El using namespace std se usa para no tener que escribir std:: antes de elementos que pertenecen al espacio de nombres
 
 int main(){
-    const int a = 120, b = 100, c = 80 ; //Aqui definimos las variables de velocidad de cada hechizo como usualmente lo hariamos, pero en este caso definimos "cons" lo cual evita que el valor de la variable sea modificada dentro del programa
+    const int a = 120, b = 100, c = 80 ; //Aqui definimos las variables de velocidad de cada hechizo como usualmente lo hariamos, pero en este caso definimos "cons" lo cual evita que el valor de la variable sea modificada dentro del programa 
     int x;
-    double d;
+    int d;
     
     cout<<"Mago de Aetheria, has entrado en una pelea contra unos enemigos de la tierra"<<endl;
 
     cout<<"¿A que distancia se encuentra tu enemigo?"<<endl;
     cout<<"Distancia: ";
     cin>>d;
-
-    if (typeid(d)!=typeid(int)){
-        cout<<"No puedes ingresar letras"<<endl;
-    } //Este bloque de codigo se usa para evitar errores de codigo y dado el caso cerrar el programa
-
-    else{
-    
-        int e=0;
+    int e=0;
         
-        while (e<1 || e>3){ //Este bloque de codigo hace que el usuario solo sea incapaz de digitar opciones incorrectas.
+    while (e<1 || e>3){ //Este bloque de codigo hace que el usuario solo sea incapaz de digitar opciones incorrectas.
     
-            cout<<"Elige un hechizo para combatir"<<endl;
-            cout<<"1. Hechizo de fuego (Largo alcance)"<<endl;
-            cout<<"2. Hechizo de hielo (Largo alcance)"<<endl;
-            cout<<"3. Hechizo de veneno (Largo alcance)"<<endl;
+        cout<<"Elige un hechizo para combatir"<<endl;
+        cout<<"1. Hechizo de fuego (Largo alcance)"<<endl;
+        cout<<"2. Hechizo de hielo (Largo alcance)"<<endl;
+        cout<<"3. Hechizo de veneno (Largo alcance)"<<endl;
     
-            cin>>e;
-            if(e == 1){
+        cin>>e;
+        if(e == 1){
         
-                cout<<"Has elegido el hechizo de fuego"<<endl;
-                cout<<"Disparando el hechizo de fuego"<<endl;
-                x = d/a;
-                cout<<"El hechizo de fuego ha demora en impactar al enemigo en "<<x<<" segundos";
-            }
-            else if(e == 2){
-                cout<<"Has elegido el hechizo de hielo"<<endl;
-                cout<<"Disparando el hechizo de hielo"<<endl;
-                x = d/b;
-                cout<<"El hechizo de hielo ha demora en impactar al enemigo en "<<x<< " segundos";
-            }
-            else if(e == 3){
-                cout<<"Has elegido el hechizo de veneno"<<endl;
-                cout<<"Disparando el hechizo de veneno"<<endl;
-                x = d/c;
-                cout<<"El hechizo de veneno ha demora en impactar al enemigo en "<<x<< " segundos";
-            }
-            else{
-                cout<<"Elige un hechizo valido"<<endl;
-            }
+            cout<<"Has elegido el hechizo de fuego"<<endl;
+            cout<<"Disparando el hechizo de fuego"<<endl;
+            x = d/a;
+            cout<<"El hechizo de fuego ha demora en impactar al enemigo en "<<x<<" segundos";
         }
-    }    
+        else if(e == 2){
+            cout<<"Has elegido el hechizo de hielo"<<endl;
+            cout<<"Disparando el hechizo de hielo"<<endl;
+            x = d/b;
+            cout<<"El hechizo de hielo ha demora en impactar al enemigo en "<<x<< " segundos";
+        }
+        else if(e == 3){
+            cout<<"Has elegido el hechizo de veneno"<<endl;
+            cout<<"Disparando el hechizo de veneno"<<endl;
+            x = d/c;
+            cout<<"El hechizo de veneno ha demora en impactar al enemigo en "<<x<< " segundos";
+        }
+        else{
+            cout<<"Elige un hechizo valido"<<endl;
+        }
+    }
   return 0;
 }
