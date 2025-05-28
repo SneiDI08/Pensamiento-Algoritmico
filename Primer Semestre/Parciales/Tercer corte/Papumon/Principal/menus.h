a1e8f9b829c7ca3bc546b7cdcffd3da7c6ff5ce3
SneiDI08 <sneidi200810@gmail.com>
2025-05-27 23:03:48 -0500

    Create menus.h

---
+++ Primer Semestre/Parciales/Tercer corte/Papumon/Principal/menus.h
@@ -0,0 +1,15 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+#include <vector>
+#include <string>
+#include "clases.h" 
+
+using std::vector;
+using std::string;
+
+void menu_principal();
+vector<papumon> menu_seleccion(const string& jugador);
+void menu_batalla(vector<papumon>& equipo1, vector<papumon>& equipo2);
+
+#endif
