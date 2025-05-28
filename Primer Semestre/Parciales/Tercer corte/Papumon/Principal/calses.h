07a60a1e1fa2f8425e67d2a18b83001d7d2ce7e6
SneiDI08 <sneidi200810@gmail.com>
2025-05-27 23:05:52 -0500

    Create calses.h

---
+++ Primer Semestre/Parciales/Tercer corte/Papumon/Principal/calses.h
@@ -0,0 +1,36 @@
+#ifndef CLASES_H
+#define CLASES_H
+
+#include <string>
+#include <vector>
+
+class ataque {
+public:
+    std::string nombre;
+    int poder;
+	int uso;
+
+    ataque(std::string nombre_ataque, int poder_ataque, int uso);
+};
+
+class papumon {
+public:
+    std::string nombre;
+    std::string tipo;
+    int vida;
+    int defensa;
+    int aguante;
+    std::vector<ataque> ataques;
+
+    papumon(std::string nombre, std::string tipo, int vida, int defensa, int aguante, std::vector<ataque> ataques);
+};
+
+
+std::vector<ataque> ataques_viento();
+std::vector<ataque> ataques_fuego();
+std::vector<ataque> ataques_agua();
+std::vector<ataque> ataques_tierra();
+std::vector<ataque> ataques_normal();
+std::vector<papumon> papumones();
+
+#endif
